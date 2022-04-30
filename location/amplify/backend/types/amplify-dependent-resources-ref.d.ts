43cda3ef15aad0e2f7dbe64f15c3ce9fe95f31e6
Alexander Perinetti <alexperinetti@gmail.com>
2022-04-30 18:05:13 -0400

    Attempt to display AWS geo map

---
+++ location/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "locationservicebc8c23d3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "geo": {
+        "map6918838c": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
+        }
+    }
+}
--- location/src/App.js
+++ location/src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+
 import './App.css';
 
+import Amplify, {Auth} from 'aws-amplify';
+import {Signer} from '@aws-amplify/core';
+import awsconfig from './aws-exports';
+
+import ReactMapGL, {
+  NavigationControl,
+} from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const mapName = 'HeadstarterMap';
+
+Amplify.configure(awsconfig);
+
+const transformRequest = (credentials) => (url, resourceType) => {
+  if (resourceType === "Style" && !url?.includes("://")) {
+    url = `https://maps.geo.${awsconfig.aws_project_region}.amazonaws.com/maps/v0/${url}/style-descriptor`;
+  }
+  if (url?.includes("amazonaws.com")) {
+    return {
+      url: Signer.signUrl(url, {
+        access_key: credentials.accessKeyId,
+        secret_key: credentials.secretAccessKey,
+        session_token: credentials.sessionToken,
+      })
+    };
+  }
+
+  return {url: url || ""};
+};
+
 function App() {
+  const [credentials, setCredentials] = useState(null);
+
+  const [viewport, setViewport] = useState({
+    longitude: 100,
+    latitude: 50,
+    zoom: 1,
+  });
+
+  useEffect(() => {
+    const fetchCredentials = async () => {
+      setCredentials(await Auth.currentUserCredentials());
+    };
+    fetchCredentials();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      {credentials ? (
+        <ReactMapGL
+          {...viewport}
+          width='100%'
+          height='100vh'
+          transformRequest={transformRequest(credentials)}
+          mapStyle={mapName}
+          onViewportChange={setViewport}
         >
-          Learn React
-        </a>
-      </header>
+          <div style={{position: 'absolute', left: 20, top: 20}}>
+            <NavigationControl showCompass={false}/>
+          </div>
+        </ReactMapGL>
+      ) : (
+        <h1>Map is loading...</h1>
+      )}
     </div>
   );
 }
