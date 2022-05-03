1db927df0a9db9fa0df072a54574eb8eb6fb83cf
Alexander Perinetti <alexperinetti@gmail.com>
2022-05-03 17:58:52 -0400

    Map page now geolocates device.

--- Headstarter/LocationProject3/location-app/src/pages/Map.js
+++ Headstarter/LocationProject3/location-app/src/pages/Map.js
@@ -5,19 +5,30 @@ import { createMap } from "maplibre-gl-js-amplify";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect } from 'react';
 
-async function initializeMap() {
+async function initializeMap(coords) {
     const map = await createMap({
         container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-        center: [-73.98597609730648, 40.751874635721734], // center in New York
+        center: [coords[0], coords[1]], // center in New York
         zoom: 11,
     })
     return map;
 }
   
 function Map() {
+    let coords = [-73.98597609730648, 40.751874635721734]; //Manhattan
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            coords = [position.coords.latitude, position.coords.longitude];
+            console.log(coords);
+        },
+        function(error) {
+            console.error("Error Code = " + error.code + " - " + error.message);
+        }
+    );
+
     useEffect( () => {
         async function init() {
-        const map = await initializeMap();
+        const map = await initializeMap(coords);
       }
       init();
     }, []);
